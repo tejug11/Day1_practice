3d8f915c84157d06c6e73df550650e10648bff76
Tejal Gaikwad <you@example.com>
2022-09-08 18:58:21 +0530

    Refactored Triangle

--- Test.java
+++ Test.java
@@ -7,16 +7,19 @@ public class Test {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the length");
-		int a = sc.nextInt();
-		System.out.println("Enter the weidth");
-		int b = sc.nextInt();
-
-		int p, area;
-		p = a * b;
-		area = 2 * (a + b);
-		System.out.println("Rectangle of area = " + area);
-		System.out.println("Rectangle of perimeter " + p);
+		float a, b, c, h, area, perimeter;
+		System.out.println("Enter length of frist side of triangle ");
+		a = sc.nextFloat();
+		System.out.println("Enter length of second side of triangle ");
+		b = sc.nextFloat();
+		System.out.println("Enter length of third side of triangle ");
+		c = sc.nextFloat();
+		System.out.println("Enter hight ");
+		h = sc.nextFloat();
+		area = b * h / 2;
+		perimeter = a + b + c;
+		System.out.println("Area of Triangle  = " + area);
+		System.out.println("Perimeter of Triangle " + perimeter);
 	}
 
 }
