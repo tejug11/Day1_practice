99dfc1ab09d95beb40a6f26c699159f1054a2961
Tejal Gaikwad <you@example.com>
2022-09-08 19:06:30 +0530

    Refactored Circle

--- Test.java
+++ Test.java
@@ -6,20 +6,15 @@ public class Test {
 
 	public static void main(String[] args) {
 
+		int r;
+		double pi = 3.14, area, perimeter;
 		Scanner sc = new Scanner(System.in);
-		float a, b, c, h, area, perimeter;
-		System.out.println("Enter length of frist side of triangle ");
-		a = sc.nextFloat();
-		System.out.println("Enter length of second side of triangle ");
-		b = sc.nextFloat();
-		System.out.println("Enter length of third side of triangle ");
-		c = sc.nextFloat();
-		System.out.println("Enter hight ");
-		h = sc.nextFloat();
-		area = b * h / 2;
-		perimeter = a + b + c;
-		System.out.println("Area of Triangle  = " + area);
-		System.out.println("Perimeter of Triangle " + perimeter);
-	}
+		System.out.println("Enter radius of circle ");
+		r = sc.nextInt();
+		area = pi * r * r;
+		perimeter = 2 * pi * r;
+		System.out.println("area of circle = " + area);
+		System.out.println("Perimeter of circle = " + perimeter);
 
+	}
 }
